0eed8861887e2ee78e26bd03cd11ad60603faf80
Eugene <petrenkojene@gmail.com>
2022-10-08 10:52:16 +0300

    merge

--- add.js
+++ add.js
@@ -23,65 +23,10 @@
 //     target.style.backgroundColor = "green";
 //   }
 // });
-// function calculateVolumeAndArea(item) {
-//   if (
-//     typeof item !== "number" ||
-//     item <= 0 ||
-//     Number.isInteger(item) == false
-//   ) {
-//     console.log("При вычислении произошла ошибка");
-//   } else {
-//     let sqr = 6 * item * item;
-//     let volume = item * item * item;
-//     console.log(`Объем куба: ${sqr}, площадь всей поверхности: ${volume}`);
-//   }
-// }
-// calculateVolumeAndArea(0.2);
-// function getCoupeNumber(item) {
-//   if (
-//     typeof item !== "number" ||
-//     item < 0 ||
-//     Number.isInteger(item) === false
-//   ) {
-//     console.log("Ошибка. Проверьте правильность введенного номера места");
-//   }
-//   if (item === 0 || item > 36) {
-//     console.log("Таких мест в вагоне не существует");
-//   }
-//   let numberTiket = Math.ceil(item / 4);
-//   console.log(numberTiket);
-// }
-// getCoupeNumber(2);
-// function getTimeFromMinutes(time) {
-//   let hourse = Math.floor(time / 60);
-//   let minutes = Math.floor(time % 60);
-//   if (isNaN(time) || time < 0 || Number.isInteger(time) === false) {
-//     console.log("Ошибка, проверьте данные");
-//   } else {
-//     let a = "";
-//     if (hourse === 0 || hourse > 5) {
-//       a = "часов";
-//     }
-//     if (hourse === 1) {
-//       a = "час";
-//     }
-//     if (hourse >= 2 && hourse <= 4) {
-//       a = "часа";
-//       console.log(`Это ${hourse} ${a} ${minutes} минут`);
-//     }
-//   }
-// }
-// getTimeFromMinutes(150);
-function maxNumber(a, b, c, d) {
-  if (
-    typeof a !== "number" ||
-    typeof b !== "number" ||
-    typeof c !== "number" ||
-    typeof d !== "number"
-  ) {
-    return 0;
-  } else {
-    return Math.max(a, b, c, d);
-  }
+
+const str = [2, 35, 43, 1, 24, 6];
+
+console.log(str.sort(sortMumber));
+function sortMumber(a, b) {
+  return b - a;
 }
-maxNumber(3, 6, 9, 7);
